f3b0072f3b3060ff226cc0e673e2f428760335ea
TizzleMeDis <anthonytisdale19@gmail.com>
2021-11-19 04:50:03 -0800

    Finished updating index.html. Need to fix styling

--- D3.js/CAHousingPriceAvg.js
+++ D3.js/CAHousingPriceAvg.js
@@ -1,13 +1,9 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/@kennynguyen/bar-chart-race-with-scrubber.js?v=3";
+import define from "https://api.observablehq.com/d/8dab4cb24eb8c6ce.js?v=3";
 new Runtime().module(define, name => {
-  if (name === "chart") return new Inspector(document.querySelector("#observablehq-chart-c13324a3"));
-  if (name === "data") return new Inspector(document.querySelector("#observablehq-data-c13324a3"));
-  if (name === "bars") return new Inspector(document.querySelector("#observablehq-bars-c13324a3"));
-  if (name === "labels") return new Inspector(document.querySelector("#observablehq-labels-c13324a3"));
-  if (name === "axis") return new Inspector(document.querySelector("#observablehq-axis-c13324a3"));
-  if (name === "ticker") return new Inspector(document.querySelector("#observablehq-ticker-c13324a3"));
-  if (name === "keyframes") return new Inspector(document.querySelector("#observablehq-keyframes-c13324a3"));
-  if (name === "viewof keyframe") return new Inspector(document.querySelector("#observablehq-viewof-keyframe-c13324a3"));
-  return ["update","names","rank","datevalues","color"].includes(name);
+	if (name === "chart") return new Inspector(document.querySelector("#observablehq-chart-6c037dd6"));
+	if (name === "viewof keyframe") return new Inspector(document.querySelector("#observablehq-viewof-keyframe-6c037dd6"));
+	if (name === "data") return new Inspector(document.querySelector("#observablehq-data-6c037dd6"));
+	if (name === "names") return new Inspector(document.querySelector("#observablehq-names-6c037dd6"));
+	return ["update","rank","keyframes","ticker","datevalues","color","bars"].includes(name);
 });
---
+++ MenuBar/menuBar.js
@@ -0,0 +1,30 @@
+
+
+document.getElementById("demo").innerHTML = ("Hello World!");
+
+function menuLinks(){
+	document.write("<ul class='template'>"
+		+ "<li>Home</li>"
+		+ "<li>Projects</li>"
+		+ "<li>Contact</li>"
+		+ "<li>Git Hub</li>" 
+		+ "</ul>");
+}
+
+//changes the look of the html for different views of the navbar
+
+
+
+
+
+//sets of colors that work well with each other
+const colorWheel = {"#0B0C10", "#1F2833", "#C5C6C7", "#66FCF1", "#45A29E", 
+					"#5D001E", "#E3E2DF", "#E3AFBC", "#9A1750", "#EE4C7C", 
+					"#EDC7B7", "#EEE2DC", "#BAB2B5", "#123C69", "#AC3B61"}
+
+
+
+
+const changeColor = (header, body, containers, accents, text) = {
+	//This is where you add the color and it will link to the dom of the css
+}

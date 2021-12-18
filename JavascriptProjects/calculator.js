eee8fa2eb66865aa40bcb0131e5ee00d5395f920
TizzleMeDis <54555309+TizzleMeDis@users.noreply.github.com>
2021-12-18 02:35:01 -0800

    Making javascript calculator

---
+++ JavascriptProjects/calculator.js
@@ -0,0 +1,38 @@
+class Calculator {
+
+	constructor(previousOperandTextElement, currentOperandTextElement) {
+		this.previousOperandTextElement = previousOperandTextElement;
+		this.currentOperandTextElement = currentOperandTextElement;
+	}
+
+	clear() {
+		this.previousOperandTextElement = ''
+		this.currentOperandTextElement = ''
+		this.operation
+	}
+
+	delete() {
+
+	}
+
+	appendNumber(number) {
+
+	}
+
+	chooseOperation(operation) {
+
+	}
+
+	compute() {
+
+	}
+
+	updateDisplay() {
+
+	}
+}
+const equation = [];
+
+function getEquation(press) {
+	equation.push();
+}

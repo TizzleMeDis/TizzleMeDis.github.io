7db75b436db8cff6e62406e9b4c43143dc5c39c4
TizzleMeDis <54555309+TizzleMeDis@users.noreply.github.com>
2021-12-22 17:36:56 -0800

    New additions to the calculator
    Added attributes to buttons so the javascript can target the buttons. Still working out how to make a Calculator class, and pass data to the js and html file.

--- JavascriptProjects/calculator.js
+++ JavascriptProjects/calculator.js
@@ -1,14 +1,15 @@
 class Calculator {
 
 	constructor(previousOperandTextElement, currentOperandTextElement) {
-		this.previousOperandTextElement = previousOperandTextElement;
-		this.currentOperandTextElement = currentOperandTextElement;
+		this.previousOperandTextElement = previousOperandTextElement
+		this.currentOperandTextElement = currentOperandTextElement
+		this.clear()
 	}
 
 	clear() {
-		this.previousOperandTextElement = ''
-		this.currentOperandTextElement = ''
-		this.operation
+		this.currentOperand = ''
+		this.previousOperand = ''
+		this.operation = undefined
 	}
 
 	delete() {
@@ -16,7 +17,7 @@ class Calculator {
 	}
 
 	appendNumber(number) {
-
+		this.currentOperand = number
 	}
 
 	chooseOperation(operation) {
@@ -28,6 +29,7 @@ class Calculator {
 	}
 
 	updateDisplay() {
+		this.currentOperandTextElement.innerText = this.currentOperand
 
 	}
 }
@@ -36,3 +38,20 @@ const equation = [];
 function getEquation(press) {
 	equation.push();
 }
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelector('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		console.log("clicked");
+		calculator.appendNumber(button.innerText)
+		calculator.updateDisplay();
+	})
+})

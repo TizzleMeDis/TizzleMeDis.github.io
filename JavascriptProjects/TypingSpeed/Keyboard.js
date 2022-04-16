7b5f12ab2fdbcee603d2a74500013aca857f2088
TizzleMeDis <anthonytisdale19@gmail.com>
2022-04-15 21:53:28 -0700

    Made a virtual keyboard fully functional, Needs more time with css to clean the styling

--- JavascriptProjects/TypingSpeed/Keyboard.js
+++ JavascriptProjects/TypingSpeed/Keyboard.js
@@ -1,25 +1,51 @@
 const keys = document.querySelectorAll("div[key]");
-const keyPressed = document.getElementById("key-box");
+const inputBox = document.getElementById("input-box");
 
-keyPressed.addEventListener("keypress", (event) => {
-	console.log("key pressed: " + event.key);
-	keys.forEach(key => {
+for (var i = keys.length - 1; i >= 0; i--) {
 
-		let keyBoard = event.key;
-		console.log("key board: " + keyBoard)
-		if(keyBoard.match(/${key.innerText}/i)) {
-			console.log("It worked.")
-			changeColor(key);
-		}
-	});
+	if(keys[i].hasAttribute('two-keys')) {
+		keys[i].setAttribute("keyname", keys[i].innerText.substring(0, 1));
+		keys[i].setAttribute("lowerCaseName", keys[i].innerText.substring(1, 2));
+	} else {
+		keys[i].setAttribute("keyname", keys[i].innerText);
+		keys[i].setAttribute("lowerCaseName", keys[i].innerText.toLowerCase());
+	}
+
+}
+
+inputBox.focus();
+
+inputBox.addEventListener('keydown', (event) => {
+	console.log("key: " + event.key);
+	for (var i = keys.length - 1; i >= 0; i--) {
+	 	if(keys[i].getAttribute('keyname') == event.key || keys[i].getAttribute('lowerCaseName') == event.key){
+	 		keys[i].setAttribute("class", "active");
+	 	} else if (event.key == "CapsLock" && keys[i].getAttribute("keyname") == "Caps") {
+	 		keys[i].setAttribute("class", "active");
+	 	} else if (event.key == " " && keys[i].hasAttribute("space-bar")) {
+	 		keys[i].setAttribute("class", "active");
+	 	} else if (event.key == "Control" && keys[i].hasAttribute("ctrl")) {
+	 		keys[i].setAttribute("class", "active");
+	 	}
+	}
+
+	if (event.key == "Tab") {
+		keys[14].removeAttribute("class");
+	}
 });
 
-function changeColor(key) {
-	key.style.background = "white";
-	key.style.color = "black";
-}	
+inputBox.addEventListener('keyup', (event) => {
+	for (var i = keys.length - 1; i >= 0; i--) {
+	 	if(keys[i].getAttribute('keyname') == event.key || keys[i].getAttribute('lowerCaseName') == event.key){
+	 		keys[i].removeAttribute("class", "active");
+	 	} else if (event.key == "CapsLock" && keys[i].getAttribute("keyname") == "Caps") {
+	 		keys[i].removeAttribute("class", "active");
+	 	} else if (event.key == " " && keys[i].hasAttribute("space-bar")) {
+	 		keys[i].removeAttribute("class", "active");
+	 	} else if (event.key == "Control" && keys[i].hasAttribute("ctrl")) {
+	 		keys[i].removeAttribute("class", "active");
+	 	}
+	}
+});
 
-function changeColorBack(key) {
-	key.style.background = "black";
-	key.style.color = "white";
-}
+console.log("done");
---
+++ JavascriptProjects/TypingSpeed/TypingSpeed.js
@@ -0,0 +1,12 @@
+
+const story = "From fairest creatures we desire increase, that thereby beauty’s rose might never die, " + 
+"but, as the riper should by time decease, his tender heir might bear his memory. " + 
+"But thou, contracted to thine own bright eyes, feed’st thy light’s flame with self-substantial fuel, " + 
+"making a famine where abundance lies, thyself thy foe, to thy sweet self too cruel. " + 
+"Thou that art now the world’s fresh ornament and only herald to the gaudy spring. " + 
+"Within thine own bud buriest thy content and, tender churl, mak’st waste in niggarding. " + 
+"Pity the world, or else this glutton be to eat the world’s due, by the grave and thee."
+
+
+const textArea = document.getElementById("story");
+textArea.innerText = story;

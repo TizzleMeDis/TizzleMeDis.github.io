8d680ae06c2d94808b850fb9344a3b436e988267
Anthony <54555309+TizzleMeDis@users.noreply.github.com>
2024-02-08 18:21:38 +0000

    creating new profile webpage. Need to push other projects over as well

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='m-0 p-4 px-8'>
+      <header className='flex flex-row justify-between '>
+        <a>Home</a>
+        <ul className='flex flex-row space-x-4'>
+          <li><a>About</a></li>
+          <li><a>Projects</a></li>
+          <li><a>Github</a></li>
+        </ul>
+      </header>
+
+      <main>
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
